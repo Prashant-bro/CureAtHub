babbde3f22037e9cb88798e2ce75c73e45778637
Prashant Singh <prashantsingh20102006@example.com>
2026-06-19 12:24:24 +0530

    feat: integrate Sentry SDK for client, server, and edge error monitoring

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -8,4 +10,33 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  // Suppresses source map uploading logs during bundling
+  silent: true,
+  org: "prashant-sherbahadur-singh",
+  project: "javascript-nextjs",
+}, {
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Route browser requests to Sentry through a Next.js rewrite to bypass Ad-Blockers.
+  // (Keep in mind that this increases your server traffic)
+  tunnelRoute: "/monitoring",
+
+  // Hides source maps from visitors
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Jobs.
+  // (Requires Vercel Integration)
+  automaticVercelMonitors: true,
+});
+
